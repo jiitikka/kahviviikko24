017287254532b82ba597589ada5e6b2ba1b5cf22
Jani Tikka <jani.tikka@gmail.com>
2024-03-31 15:08:51 +0300

    Added the cafe list and CafeCell component

--- app/components/CafeCell.tsx
+++ app/components/CafeCell.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Image from 'next/image'
 
-const CafeCell = props => {
+interface Props {
+    alt: string,
+    src: string
+}
+
+const CafeCell = (props: Props) => {
   return (
-    <div className='bg-slate-300 h-[200px]'>
-        <Image src={props.src} alt={props.alt} fill />
+    <div className='bg-white h-[144px] w-[144px] lg:h-[180px] lg:w-[180px]'>
+        <Image src={props.src} alt={props.alt} height={200} width={200} style={{objectFit: "contain"}} />
     </div>
   )
 }
--- app/components/CafesParticipating.tsx
+++ app/components/CafesParticipating.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import CafeCell from './CafeCell'
 
 const CafesParticipating = () => {
+  const listOfCafes = [["/cafes/asema_logo.png", "Aseman kahvila logo"], ["/cafes/hidden_logo.png", "Thé Hidden logo"], ["/cafes/kaffila_logo.png", "Kaffila logo"], ["/cafes/kahiwa_logo.png", "Kahiwa Pirkkala logo"], ["/cafes/kokko_logo.png", "Kahvibaari Kokko logo"], ["/cafes/marco_logo.png", "Boulangerie Marco logo"], ["/cafes/metso_logo.png", "Cafe Metso logo"], ["/cafes/mimosa_logo.png", "Cafe and Bakery Mimosa logo"], ["/cafes/mokkamestarit_logo.png", "Mokkamestarit logo"], ["/cafes/onda_logo.png", "Music & Arts Cafe Onda"], ["/cafes/pala_logo.png", "Cafe Pala logo"], ["/cafes/puusti_logo.png", "Cafe Puusti logo"], ["/cafes/runo_logo.png", "Kahvila Runo logo"], ["/cafes/siiri_logo.png", "Kahvila Siiri logo"], ["/cafes/vuohi_logo.png", "Vihreä vuohi logo"]];
+
+  const cafes = listOfCafes.map((cafe) => 
+  <CafeCell alt={cafe[1]} src={cafe[0]} />);
+
   return (
-    <div className='h-screen bg-beans-bg-image bg-cover'>
-      <div className=' bg-white opacity-80 bg-blend-lighten h-full w-full absolute'></div>
+    <div className='flex flex-row flex-wrap gap-x-6	gap-y-6 lg:gap-16 text-white p-4 md:p-10 bg-beans-bg-image bg-cover'>
+      <h1 className='flex-initial max-w-96 text-6xl md:text-8xl'>Mukana olevat kahvilat</h1>
+      <div className='flex flex-row w-full flex-wrap md:w-2/3'>
+        {cafes}
+      </div>
+
+      {/* <div className=' bg-white opacity-80 bg-blend-lighten h-full w-full absolute'></div> */}
+
     </div>
   )
 }
