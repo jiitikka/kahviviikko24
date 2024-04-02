6fb71bd82a414f77e3464b0d6fa7082856e3a70b
Jani Tikka <jani.tikka@gmail.com>
2024-04-02 13:39:10 +0300

    Added links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,15 +5,15 @@ const Footer = () => {
   return (
     <div className='w-full flex flex-row flex-wrap items-center justify-center bg-brand-pink text-white gap-4 lg:gap-10 p-4 lg:p-10'>
       <a href='https://instagram.com/tampereenkahviviikko'>
-        <Image className="h-12 w-12 rounded-full border-2 border-brand-pink hover:border-white" src="/Instagram.svg" alt="Link to our Instagram page" height={48} width={48}/>
+        <Image className="h-12 w-12 rounded-full border-2 border-brand-pink hover:border-white transition-colors" src="/Instagram.svg" alt="Link to our Instagram page" height={48} width={48}/>
       </a>
       <a href='https://facebook.com/tampereenkahviviikko'>
-        <Image className='h-12 w-12 rounded-full border-2 border-brand-pink hover:border-white' src="/Facebook.svg" alt="Link to our Instagram page" height={48} width={48}/>
+        <Image className='h-12 w-12 rounded-full border-2 border-brand-pink hover:border-white transition-colors' src="/Facebook.svg" alt="Link to our Instagram page" height={48} width={48}/>
       </a>
       <p>Yhteydenotot: tampereenkahviviikko(at)gmail.com</p>
       <p>Taustakuvat: Unsplash</p>
       <p>© Jani Tikka/Tampereen kahviviikko 2024</p>
-      <a className='flex justify-center align-middle items-center rounded-full m-2 border-2 border-brand-pink hover:border-white hover:text-brand-pink' href='#home'>
+      <a className='flex justify-center align-middle items-center rounded-full m-2 border-2 border-brand-pink hover:border-white hover:text-brand-pink transition-colors' href='#home'>
         <Image className='h-12 w-12 text-brand-pink' src="/top.svg" alt="Link to our top of the page" height={48} width={48}/></a>
     </div>
   )
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,11 +5,12 @@ const Hero = () => {
   return (
     <div className='z-0 h-screen bg-beans-bg-image bg-cover bg-fixed relative' id="home">
         <div className='z-10 bg-white opacity-80 bg-blend-overlay h-full w-full absolute'></div>
-        <div className='z-20 relative h-full w-full'>
-            <Image className='z-30 left-auto right-auto top-auto bottom-auto h-96 w-auto opacity-100 p-4 bg-white/50 md:bg-transparent md:p-16 p- lg:p-40' alt="Tampereen Kahviviikko Logo" src={"/TKV24 Logo.svg"} fill></Image>
-            <Image className='z-20 absolute left-0 bottom-0' alt="Beans partial right" src={"/bg bean left.svg"} width={300} height={1000}></Image>
-            <Image className='z-20 absolute right-0 top-0' alt="Beans partial left" src={"/bg bean right.svg"} width={800} height={1000}></Image>
-        </div>        
+        <Image className='z-20 absolute left-0 bottom-0' alt="Beans partial right" src={"/bg bean left.svg"} width={300} height={1000}></Image>
+        <Image className='z-20 absolute right-0 top-0' alt="Beans partial left" src={"/bg bean right.svg"} width={800} height={1000}></Image>
+        <div className='z-20 flex flex-col gap-10 h-full w-full items-center justify-center opacity-100 bg-white/50 md:bg-transparent p-4 md:p-16 lg:p-40 2xl:px-80'>
+            <Image className='z-30 h-auto w-auto max-w-7xl' width={1121} height={640} alt="Tampereen Kahviviikko Logo" src={"/TKV24 Logo.svg"}></Image>
+            <a className='z-30 py-2 px-8 border-2 border-black bg-black rounded-full text-white hover:text-black hover:bg-white text-1xl transition-colors animate-pulse' href='#about'>Lue lisää</a>        
+        </div>
     </div>
   )
 }
