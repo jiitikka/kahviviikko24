767a738a479c8009bb64efc2ceafcf56d33ff4fa
Jani Tikka <jani.tikka@gmail.com>
2024-04-03 09:54:57 +0300

    Hotjar variable creataed

--- app/scripts/HotjarSnippet.tsx
+++ app/scripts/HotjarSnippet.tsx
@@ -1,17 +1,15 @@
-"use client"
 import React from 'react'
 import Script from 'next/script';
 
 const HotjarSnippet = () => {
-    const HOTJAR_ID = 1717688;
-    if (HOTJAR_ID) return null
+    if (process.env.HOTJAR_ID) return null
     return (
         <div>
             <Script id="hotjar-snippet">
             {`
                 (function(h,o,t,j,a,r){
                     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                    h._hjSettings={hjid:${HOTJAR_ID},hjsv:6};
+                    h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
                     a=o.getElementsByTagName('head')[0];
                     r=o.createElement('script');r.async=1;
                     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
